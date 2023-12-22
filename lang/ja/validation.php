c97ae43402bce94d3616bd6d6d1e3b7a805be001
inabe350 <KentaInabe03@gmail.com>
2023-12-22 18:14:54 +0900

    商品管理

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -153,6 +153,12 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'title' => '件名',
+        'content' => '本文',
+        'name' => '商品名',
+        'memo' => 'メモ',
+        'price' => '商品価格',
+    ],
 
 ];
